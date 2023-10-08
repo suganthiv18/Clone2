de270747fd568ea3d32aad8d0c9db16ed5e851bc
suganthiv18 <143151933+suganthiv18@users.noreply.github.com>
2023-10-07 22:31:33 -0400

    Update TestRunner.java

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -1,24 +1,42 @@
 package Runner;
 
+import org.junit.runner.RunWith;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 
-//import com.beust.jcommander.Parameters;
-
+import Utilities.ConfigReader;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin = { "pretty","html:target/dsAlgoReport.html", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, // reporting purpose
-monochrome = false, // console output
-tags = "", // tags from feature file
-features = { "src/test/resources/features" }, // location of feature files
-glue = { "StepDefinition", "Hooks" }) // location of step definition files
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/Features",
+glue={"StepDefinitions","Hooks"},
+plugin = {"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+		"html:Reports/Cucumber-Reports/Reports.html"},
 
-public class TestRunner extends AbstractTestNGCucumberTests {
-	//@Parameters({"browser","url"})
-	@DataProvider(parallel = false)
-	public Object[][] scenarios() {
-	
-		return super.scenarios();
-		}
+dryRun=false,
+monochrome = true)
+
+public class TestRunner{
 
 }
+
+//public class TestRunner extends AbstractTestNGCucumberTests {
+	
+//	@Override
+//	@DataProvider(parallel = false)
+//	public Object[][]scenarios(){
+//		return super.scenarios();
+//	}
+//	
+//	@BeforeTest
+//	@Parameters("browser")
+//	public void beforeClass(String browser){
+//		ConfigReader.setBrowserType("browser");
+//	}
+//}
